7d8b6215bc5608ad95c4e9c1bf4efd4ebde44ff6
Daniyal Ganiuly <daniyal.ganiuly@gmail.com>
2023-02-06 15:42:47 +0600

    initial commit

---
+++ weather_api_06.02/weather_api_06_02App.swift
@@ -0,0 +1,17 @@
+//
+//  weather_api_06_02App.swift
+//  weather_api_06.02
+//
+//  Created by Daniyal Ganiuly on 06.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct weather_api_06_02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
